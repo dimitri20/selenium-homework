111bd8f3124d04c7f9636a62a9fab9dcd90147a5
dimitri20 <dimitri.gulua@geolab.edu.ge>
2021-06-19 20:38:05 +0400

    add WaitsTest

---
+++ src/test/java/WaitsTest.java
@@ -0,0 +1,31 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class WaitsTest {
+
+    @Test
+    public void main(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://demoqa.com/progress-bar");
+
+        WebElement start_button = driver.findElement(By.id("startStopButton"));
+        start_button.click();
+
+        WebElement progress_bar = driver.findElement(By.cssSelector("#progressBar > div[role='progressbar']"));
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.attributeToBe(progress_bar, "aria-valuenow", Integer.toString(100)));
+        System.out.println(progress_bar.getText());
+
+        driver.quit();
+    }
+
+}
